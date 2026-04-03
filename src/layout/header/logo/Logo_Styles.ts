44e5c8cce62b7bf2014c65f5a120d63a52d3e2f8
orlovaDev <anastasiya.orlova1305@gmail.com>
2026-04-03 19:17:51 +0300

    refactorMobileLogo

--- src/layout/header/logo/Logo_Styles.ts
+++ src/layout/header/logo/Logo_Styles.ts
@@ -1,10 +1,21 @@
 import styled from "styled-components";
 
 const Logo = styled.a`
+  background: linear-gradient(135deg, #bad1f7 0%, #4a82da 100%);
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
   display: flex;
   align-items: center;
   justify-content: center;
-`
+
+  svg {
+    width: 40px;
+    height: 40px;
+    path {
+      fill: #252728; 
+    }
+  }`
 export const S = {
   Logo
 }
